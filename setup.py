f103bb3b24d9a23aa6f730eaff2e189d5ced55fa
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-02-25 14:07:42 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'sam',
+    author_email= 'buzzworm47@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
